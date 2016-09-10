d51caae767e10d3a779ce2ec00c4f44cb82d0af0
pashim <pashim@MININT-FQBVDC2>
2016-09-10 22:36:01 +0700

    Add project files.

---
+++ device-identity/Program.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace device_identity
+{
+    class Program
+    {
+
+        private static RegistryManager registryManager;
+        private static bool isConnected = false;
+
+        private static void Main(string[] args)
+        {
+            string connectionString;
+            int userInput;
+
+            // get the connection string from arg or prompt user for input.
+            if (args[0].Length > 0)
+            {
+                connectionString = args[0];
+            }
+            else
+            {
+                Console.Write("Enter a connection string to Azure IoT Hub: ");
+                connectionString = Console.ReadLine();
+            }
+
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+
+            OpenConnection().Wait();
+            ListDevices().Wait();
+
+            do
+            {
+                userInput = UserPrompt();
+                switch (userInput)
+                {
+                    case 1:
+                        ListDevices().Wait();
+                        break;
+                    case 2:
+                        GetDeviceInformation().Wait();
+                        break;
+                    case 3:
+                        AddDevice().Wait();
+                        break;
+                    case 4:
+                        RemoveDevice().Wait();
+                        break;
+                    default:
+                        break;
+                }
+            } while (userInput != 0);
+
+            // close the connection if opened.
+            if (!isConnected) return;
+            try
+            {
+                registryManager.CloseAsync().Wait();
+                isConnected = false;
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"An error occurred while closing connection from Azure IoT Hub (message: {ex.Message}).");
+                Console.ResetColor();
+            }
+        }
+        
+        public static int UserPrompt()
+        {
+            int result;
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("1. List devices in the list: ");
+            Console.WriteLine("2. Get device information: ");
+            Console.WriteLine("3. Add a device: ");
+            Console.WriteLine("4. Remove a device: ");
+            Console.WriteLine("------------------------------");
+            Console.Write("Enter your choice ('0' to exit the program): ");
+            var input = Console.ReadLine();
+            Console.WriteLine();
+            return int.TryParse(input, out result) ? result : -100;
+        }
+
+        /// <summary>
+        /// Open a connection to Azure IoT Hub.  
+        /// </summary>
+        /// <returns></returns>
+        public static async Task OpenConnection()
+        {
+            if (isConnected == true) Console.WriteLine("Connection has been established already.");
+            else
+            {
+                try
+                {
+                    await registryManager.OpenAsync();
+                    Console.WriteLine($"Connection is now established to Azure IoT Hub...");
+                    isConnected = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"An error occurred while opening connection to Azure IoT Hub (message: {ex.Message}).");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        public static async Task ListDevices()
+        {
+            if (isConnected == true)
+            {
+                var devices = await registryManager.GetDevicesAsync(1000);
+                var deviceList = devices as IList<Device> ?? devices.ToList();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"{deviceList.Count<Device>()} device(s) found in the registry.");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                foreach (var device in deviceList)
+                {
+                    Console.WriteLine($"Device ID: {device.Id}");
+                    Console.WriteLine($"Device Key: {device.Authentication.SymmetricKey.PrimaryKey}");
+                    Console.WriteLine($"Device Status: {device.Status}");
+                    Console.WriteLine();
+                }
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Not connected to Azure IoT Hub.");
+                Console.ResetColor();
+            }
+            Console.WriteLine();
+        }
+
+        public static async Task AddDevice()
+        {
+            if (isConnected)
+            {
+                Console.Write("Enter device ID to be added: ");
+                var deviceId = Console.ReadLine();
+                var device = new Device(deviceId);
+                try
+                {
+                    device = await registryManager.AddDeviceAsync(device);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"A device '{deviceId}' has been successfully added to the IoT Hub.");
+                    Console.WriteLine($"Device ID: {device.Id}");
+                    Console.WriteLine($"Generation ID: {device.GenerationId}");
+                    Console.WriteLine($"Primary Key: {device.Authentication.SymmetricKey.PrimaryKey}");
+                    Console.WriteLine($"Secondary Key: {device.Authentication.SymmetricKey.SecondaryKey}");
+                    Console.ResetColor();
+                }
+                catch (DeviceAlreadyExistsException)
+                {
+                    device = await registryManager.GetDeviceAsync(deviceId);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"A device '{deviceId}' already exits in the registry.");
+                    Console.WriteLine($"Device ID: {device.Id}");
+                    Console.WriteLine($"Generation ID: {device.GenerationId}");
+                    Console.WriteLine($"Primary Key: {device.Authentication.SymmetricKey.PrimaryKey}");
+                    Console.WriteLine($"Secondary Key: {device.Authentication.SymmetricKey.SecondaryKey}");
+                    Console.ResetColor();
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"An error while adding a device to the registry (message: {ex.Message}).");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        public static async Task RemoveDevice()
+        {
+            if (isConnected)
+            {
+                Console.Write("Enter a device ID to be removed from the IoT Hub: ");
+                var deviceId = Console.ReadLine();
+
+                try
+                {
+                    await registryManager.RemoveDeviceAsync(deviceId);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"{deviceId} was successfully removed from the IoT hub registry.");
+                    Console.WriteLine();
+                    Console.ResetColor();
+                    await ListDevices();
+                }
+                catch (DeviceNotFoundException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"{deviceId} was not found in the IoT hub registry.");
+                    Console.ResetColor();
+                }
+            }
+
+        }
+
+        public static async Task GetDeviceInformation()
+        {
+            if (isConnected)
+            {
+                Console.Write("Enter a device ID to retrive from IoT Hub registry: ");
+                var deviceId = Console.ReadLine();
+
+                try
+                {
+                    var device = await registryManager.GetDeviceAsync(deviceId);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Device detail for:  '{deviceId}' ");
+                    Console.WriteLine($"\tDevice ID: {device.Id}");
+                    Console.WriteLine($"\tEtag: {device.ETag}");
+                    Console.WriteLine($"\tGeneration ID: {device.GenerationId}");
+                    Console.WriteLine($"\tConnection state: {device.ConnectionState}");
+                    Console.WriteLine($"\tStatus reason: {device.StatusReason}");
+                    Console.WriteLine($"\tC2D message count: {device.CloudToDeviceMessageCount}");
+                    Console.WriteLine($"\tPrimary Key: {device.Authentication.SymmetricKey.PrimaryKey}");
+                    Console.WriteLine($"\tSecondary Key: {device.Authentication.SymmetricKey.SecondaryKey}");
+                    Console.ResetColor();
+
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("An error occurred while retrieving device information from the IoT hub registry.  Make sure that the device exists in the registry.");
+                    Console.ResetColor();
+
+                }
+
+            }
+            else {}
+        }
+    }
+}
---
+++ device-simulator/Program.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+
+namespace device_simulator
+{
+    public class Parameters
+    {
+        public string LED { get; set; }
+        public string MSG_CONTROL { get; set; }
+    }
+
+ 
+    public class Command
+    {
+        public string Name { get; set; }
+        public string MessageId { get; set; }
+        public string CreatedTime { get; set; }
+        public string UpdatedTime { get; set; }
+        public object Result { get; set; }
+        public object ErrorMessage { get; set; }
+        public Parameters Parameters { get; set; }
+    }
+    class Program
+    {
+        private static string deviceId;
+        private static string hostName;
+
+        private static DeviceClient deviceClient;
+        private static readonly Random random = new Random();
+        private static bool isConnected = false;
+        private static bool isListening = false; 
+        private static bool isRemoteMonitoringDevice;
+        private static int messageCount = 0;
+        private static string deviceConnectionString;
+        private static void Main(string[] args)
+        {
+            // do not want to hard code connection string here when it is going to public github.
+            // check if cli argument exists.  if yes then arg[0] is assigned to device connection string, otherwise user gets prompted to enter the string. 
+            PrepareDeviceConnectionString(args.Length == 0 ? null : args[0]);
+            int userInput;
+
+            do
+            { 
+                userInput = UserPrompt();
+
+                switch (userInput)
+                {
+                    // opens a device connection to iot hub.
+                    case 1: Open().Wait(); break;
+
+                    // sends a telemetry once to the iot hub.  if it is a device for Remote Monitoring then it once sends deviceInfo to enable the device.
+                    case 2: Send().Wait(); break;
+                    // sends given number of telemetries at given interval.  lazy coding here - make sure you check if inputs are integer.
+                    case 3: 
+                        Console.Write("Please enter number of telemetries to be send: ");
+                        var number = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Please enter interval between telemetries (in seconds): ");
+                        var interval = Convert.ToInt32(Console.ReadLine());
+
+                        for (var i = 0; i < number; i++)
+                        {
+                            Send().Wait();
+                            Thread.Sleep(interval*1000);
+                        }
+                        break;
+                    // sets to receiving mode (could use Task.Run instead)  
+                    case 4: Receive().Wait(); break;
+                    // closes and disposes the opened connection / deviceClient 
+                    case 5: Close().Wait(); break;
+                    // prompts to re-enter device connection string
+                    case 6: PrepareDeviceConnectionString(null); break;
+                    // invalid entry (other than integer) detected
+                    case -100: Console.WriteLine("Invalid option..."); break;
+                    default: break;
+                }
+
+            } while (userInput != 0);
+        }
+
+        private static int UserPrompt()
+        {
+            int result;
+
+            var connectionStatus = isConnected ? "Connected" : "Disconnected";
+            var listeningStatus = isListening ? "Listening" : "Not Listening";
+
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("1. Connect to IoT Hub: ");
+            Console.WriteLine("2. Send a telemetry: ");
+            Console.WriteLine("3. Send multiple telemetries: ");
+            Console.WriteLine("4. Start listening for commands: ");
+            Console.WriteLine("5. Disconnect from IoT hub: ");
+            Console.WriteLine("6. Reset device connection string: ");
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"Connection Status: {connectionStatus}");
+            Console.WriteLine($"Listening Status: {listeningStatus}");
+            Console.ResetColor();
+
+            Console.WriteLine("------------------------------");
+            Console.Write("Enter your choice ('0' to exit the program): ");
+            var input = Console.ReadLine();
+            Console.WriteLine();
+            return int.TryParse(input, out result) ? result : -100;
+        }
+
+        private static void PrepareDeviceConnectionString(string argument)
+        {
+            Console.Write("Enter a connection string for this device: ");
+            deviceConnectionString = argument ?? Console.ReadLine();
+            Console.WriteLine($"Connection string: {deviceConnectionString}");
+
+            Console.Write("Is this RM device ('y' or 'n')? ");
+            var input = Console.ReadLine();
+            if (input != null) isRemoteMonitoringDevice = (input.ToUpper() == "Y");
+            else isRemoteMonitoringDevice = false;
+
+            try
+            {
+                
+                var properties = deviceConnectionString?.Split(';');
+                hostName = properties?[0].Split('=')[1];
+                deviceId = properties?[1].Split('=')[1];
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Host Name: {hostName}");
+                Console.WriteLine($"Device Id: {deviceId}");
+                Console.ResetColor();
+            }
+            catch (Exception)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("An error occurred while preparing a device connection string...");
+                Console.ResetColor();
+            }
+        }
+
+        /// <summary>
+                /// simulates sensor readings.  if this is for Remote Monitoring and message count is 0 then the device meta data is sent to the hub.
+                /// </summary>
+                /// <returns></returns>
+            public static
+            string Read()
+        {
+            var metadata = new {
+                IsSimulatedDevice = false,
+                Version = "1.0",
+                ObjectType = "DeviceInfo",
+                DeviceProperties = new {
+                    DeviceID = deviceId,
+                    HubEnabledState = true,
+                    CreatedTime = DateTime.UtcNow,
+                    DeviceState = "normal",
+                    UpdatedTime = DateTime.UtcNow,
+                    Manufacturer = "Micrsoft Global Black-belt",
+                    ModelNumber = "MD-996",
+                    SerialNumber = "SER5102",
+                    FirmwareVersion = "1.96",
+                    Platform = "Plat-99",
+                    Processor = "i3-7340",
+                    InstalledRAM = "128 MB",
+                    Latitude = 47.659159,
+                    Longitude = -122.141515
+                },
+                Commands = new[] {
+                    new  { 
+                        Name = "ControlTelemetry",
+                        Parameters = new [] {
+                            new {
+                                Name = "LED",
+                                Type = "string"
+                            },
+                            new {
+                                Name = "MSG_CONTROL",
+                                Type = "string"
+                            }
+                        }
+                    }
+                }
+            };
+
+            var sensordata = new {
+                DeviceID = deviceId,
+                TimeStamp = DateTime.Now.ToString(CultureInfo.CurrentCulture),
+                Temperature = 18.0 * random.NextDouble() + 25.0,
+                Humidity = 55.0 * random.NextDouble() + 80.0,
+                WindSpeed = 20 * random.NextDouble() + 35.0,
+                Pressure = 95 * random.NextDouble() + 125.0,
+                Latitude = 47.659159,
+                Longitude = -122.141515
+            };
+            var telemetry = (messageCount < 1 && isRemoteMonitoringDevice) ?  JsonConvert.SerializeObject(metadata) : JsonConvert.SerializeObject(sensordata);
+            return telemetry;
+        }
+
+        public static async Task Open()
+        {
+            if (isConnected == false)
+            {
+                try
+                {
+                    deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString);
+                    await deviceClient.OpenAsync();
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"{deviceId} is connected to Iot hub: {hostName}.");
+                    Console.ResetColor();
+                    isConnected = true;
+                }
+                catch (Exception ex)
+                {
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"An error occurred while opening connection to IoT hub. Possible that the connection string is malformed (message: {ex.Message}).");
+                    Console.ResetColor();
+                    isConnected = false;
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{deviceId} is already connected to IoT hub.");
+                Console.ResetColor();
+            }
+        }
+
+        public static async Task Close()
+        {
+            if (isConnected)
+            {
+                try
+                {
+                    await deviceClient.CloseAsync();
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"{deviceId} is now disconnected from IoT Hub.  Please re-connect to send telemetries.");
+                    Console.ResetColor();
+                    isConnected = false;
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"An error occurred while closing connection from IoT hub (message: {ex.Message}).");
+                    Console.ResetColor();
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"The connection is already closed.");
+                Console.ResetColor();
+            }
+        }
+
+        public static async Task Send()
+        {
+            try
+            {
+                var sensordata = Read();
+                var message = new Message(Encoding.UTF8.GetBytes(sensordata));
+                await deviceClient.SendEventAsync(message);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"{sensordata.Length} byte(s) sent: {sensordata}");
+                Console.ResetColor();
+                messageCount = messageCount + 1;
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"An error occurred while sendong telemetry to IoT hub (message: {ex.Message}");
+                Console.ResetColor();
+            }
+        }
+
+        public static async Task Receive()
+        {
+            if (isConnected)
+            {
+                if (isListening)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"The device {deviceId} is already listending to message(s) from hub...");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Listening to incomming messages from {hostName}");
+                    string msg_control = null;
+
+                    while (msg_control != "STOP") 
+                    {
+                        try
+                        {
+                            var received = await deviceClient.ReceiveAsync();
+                            isListening = true;
+                            if (received == null) continue;
+                            await deviceClient.CompleteAsync(received);
+
+                            var message = Encoding.ASCII.GetString(received.GetBytes());
+                            var cmd = JsonConvert.DeserializeObject<Command>(message);
+
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            Console.WriteLine($"Received message ({message.Length} byte(s)): {message}");
+
+                            var led_control = cmd.Parameters.LED;
+                            if (cmd.Parameters.MSG_CONTROL == "STOP")
+                            {
+                                msg_control = "STOP";
+                                isListening = false;
+                                Console.ForegroundColor = ConsoleColor.Magenta;
+                                Console.WriteLine("STOP message has arrived.  Listening has stopped.");
+                                Console.ResetColor();
+                                break;
+                            }
+
+                            // a bit of control logic for fun
+                            switch (led_control)
+                            {
+                                case "RED":
+                                    Console.BackgroundColor = ConsoleColor.Red;
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    Console.WriteLine("RED LED turned on!!");
+                                    Console.ResetColor();
+                                    break;
+                                case "GREEN":
+                                    
+                                    Console.BackgroundColor = ConsoleColor.Green;
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.WriteLine("Green LED turned on!!");
+                                    Console.ResetColor();
+                                    break;
+                                case "YELLOW":
+                                    Console.BackgroundColor = ConsoleColor.Yellow;
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.WriteLine("Yellow LED turned on!!");
+                                    Console.ResetColor();
+                                    break;
+                            }
+
+                            Console.ResetColor();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"An error occurred while reading telemetry (commands) from IoT hub (message: {ex.Message}).");
+                            Console.ResetColor();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"The device {deviceId} is not connected to IoT hub.  Connect before receiving messsage(s).");
+                Console.ResetColor();
+
+            }
+        }
+    }
+}
